50403daa879554089f9dcba2fe5e47dd5de2b84d
pallavi-pannu-oc <67274173+pallavi-pannu-oc@users.noreply.github.com>
2021-06-09 16:30:49 +0530

    Update multimodel-serving.py

--- multimodel-serving.py
+++ multimodel-serving.py
@@ -6,20 +6,13 @@ import kfp
 search_path = os.path.dirname(os.path.abspath(__file__)) + "/../components"
 component_store = kfp.components.ComponentStore(local_search_paths=[search_path])
 
-
-storage_op = component_store.load_component("storage")
 dkube_serving_op = component_store.load_component("serving")
 
 @kfp.dsl.pipeline(
     name='multimodel-pl',
     description='sample multimodel pipeline with dkube components'
 )
-def multimodel_pipeline(model):
-    with kfp.dsl.ExitHandler(exit_op=storage_op("reclaim", namespace="kubeflow", uid="{{workflow.uid}}")):
-        model_volume = json.dumps(["{{workflow.uid}}-model@model://" + str(model)])
-        storage = storage_op("export", namespace="kubeflow", input_volumes=model_volume)
-
-        list_models = kfp.dsl.ContainerOp(name="list-models",image="alpine",command="bash",arguments=["-c", "ls /model"],
-                                        pvolumes={"/model": kfp.dsl.PipelineVolume(pvc="{{workflow.uid}}-model")}})
-
+def multimodel_pipeline(model,model_mount_path):
+    
+        list_models = kfp.dsl.ContainerOp(name="list-models",image="alpine",command="bash",arguments=["-c", "ls /model_mount_path"]
         serving = dkube_serving_op(model = str(model) , device='cpu', serving_image='{"image":"ocdr/inf-multimodel:latest"}')

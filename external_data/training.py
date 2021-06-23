49915686cfcd763693c20845b666c3b9aac26a87
pallavi-pannu-oc <67274173+pallavi-pannu-oc@users.noreply.github.com>
2021-06-23 15:40:52 +0530

    Update training.py

--- external_data/training.py
+++ external_data/training.py
@@ -8,7 +8,7 @@ train_path = "/featureset/train"
 train = DkubeFeatureSet.read_features(train_path)
 train_data = pd.DataFrame(train)
 x_train=train_data.iloc[:,:-1].values
-y_train=train_data.iloc[:,13].values
+y_train=train_data.iloc[:,12].values
 rf = RandomForestClassifier(n_estimators = 1000, random_state = 1)
 rf.fit(x_train, y_train)
 ######## Model Saving ########

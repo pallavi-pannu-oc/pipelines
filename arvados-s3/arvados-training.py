f99fdba3df79c3470775d33f88ef4e147940b677
pallavi-pannu-oc <67274173+pallavi-pannu-oc@users.noreply.github.com>
2021-06-25 15:54:47 +0530

    Update arvados-training.py

--- arvados-s3/arvados-training.py
+++ arvados-s3/arvados-training.py
@@ -13,10 +13,11 @@ train_path = "/featureset/train"
 
 train = DkubeFeatureSet.read_features(train_path)
 arv_data = pd.DataFrame(train)
+x_train=arv_data.iloc[:,:-1].values
 y_values = [0 if i % 2 == 0 else 1 for i in range(1, 91)]
 ### Training the model ###
 arv_clf = RandomForestClassifier(max_depth=2, random_state=0)
-arv_clf.fit(arv_data, y_values)
+arv_clf.fit(x_train, y_values)
 
 filename = os.path.join(MODEL_DIR, "model.joblib")
 joblib.dump(arv_clf, filename)
